2d54c94e5ea300ee7031830397792feb03f5a643
AA <alexdev73@gmail.com>
2021-11-29 13:56:33 +0100

    commit

--- presentationprojet.php
+++ presentationprojet.php
@@ -1,49 +1,53 @@
 <?php require_once('config.php');
 
 $sql = "
-    SELECT name, pathFile, alt, COUNT(*)
+    SELECT nameImg, pathFile, alt, COUNT(*)
     FROM images
     WHERE pathFile LIKE '%index%'
-    GROUP BY
+    GROUP BY pathFile
+    HAVING pathFile LIKE '$parallax$'
   ";
 $pre = $pdo->prepare($sql);
 $pre->execute();
 $data = $pre->fetchAll(PDO::FETCH_ASSOC);
 foreach($data as $images){ ?>
   <?php
-    if (strpos($images['pathFile'],"parallax") != false) { ?>
-      <div class="parallax-container">
-        <div class="parallax"><img src="
-        <?php
-        echo $images['pathFile'].$images['name'];
-        ?>" alt="<?php echo $images['alt'] ?>">
+    if $data['COUNT(*)']
+      if (strpos($images['pathFile'],"parallax") != false) { ?>
+        <div class="parallax-container">
+          <div class="parallax"><img src="
+          <?php
+          echo $images['pathFile'].$images['nameImg'];
+          ?>" alt="<?php echo $images['alt'] ?>">
+          </div>
         </div>
-      </div>
-      <div class="section">
-        <div class="row container">
-          <div class="col s12 m10 l8 offset-l2 offset-m1 hoverable white" id="c+c-">
-            <h2 class="header flow-text center-align">Projects- It's more, it's less:</h2>
-            <div class="card horizontal">
-              <div class="card-image margin">
-                <img src="
-                <?php
-                if (strpos($images['pathFile'],"presprojet") != false)
-                {
-                  echo $images['pathFile'].$images['name'];
-                }
-                ?>" alt="<?php echo $images['alt'];?>">
-                <div class="card-stacked">
-                  <div class="card-content">
-                    <p>The first project was to create a "that's more that's less" in python. </p>
-                  </div>
-                  <div class="card-action">
-                    <a href="cpluscmoins.html" rel="nofollow">It's more, it's less</a>
+      <?php } ?>
+        <div class="section">
+          <div class="row container">
+            <div class="col s12 m10 l8 offset-l2 offset-m1 hoverable white" id="c+c-">
+              <h2 class="header flow-text center-align">Projects- It's more, it's less:</h2>
+              <div class="card horizontal">
+                <div class="card-image margin">
+                  <img src="
+                  <?php
+                  if (strpos($images['pathFile'],"presprojet") != false)
+                  {
+                    echo $images['pathFile'].$images['nameImg'];
+                  }
+                  ?>" alt="<?php echo $images['alt'];?>">
+                  <div class="card-stacked">
+                    <div class="card-content">
+                      <p>The first project was to create a "that's more that's less" in python. </p>
+                    </div>
+                    <div class="card-action">
+                      <a href="cpluscmoins.html" rel="nofollow">It's more, it's less</a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    <?php }
-  } ?>
+      <?php }
+    } $data['COUNT(*)'] = $data['COUNT(*)']-1;
+  }  ?>
